d5f5d60c42bd1bffed1bc2a7e3f4c248d2eff9f5
Ry0xi <ryoki0525tnk@gmail.com>
2021-11-09 15:08:17 +0900

    各メソッド名を変更

--- withPHP/user/class/UserName.php
+++ withPHP/user/class/UserName.php
@@ -2,17 +2,20 @@
 // 「ユーザー名は3文字以上15文字以下の文字列」「全て小文字で保持する」という制限をコードに落とし込む
 
 class UserName {
-  private string $name;
+  private string $value;
 
-  function __construct(string $name)
+  function __construct(string $value)
   {
-    if ($name == null) throw new Exception('ユーザー名が入力されていません。');
-    if (mb_strlen($name) < 3 || mb_strlen($name) > 15) throw new Exception('ユーザー名は3文字以上15文字以下の文字のみ利用できます。入力文字数：'.mb_strlen($name));
+    if ($value == null) throw new Exception('ユーザー名が入力されていません。');
+    if (mb_strlen($value) < 3 || mb_strlen($value) > 15) throw new Exception('ユーザー名は3文字以上15文字以下の文字のみ利用できます。入力文字数：'.mb_strlen($value));
 
-    $this->name = $name;
+    $this->value = $value;
   }
 
-  public function Value(): string {
-    return $this->name;
+  // クラスが文字列に変換される際の動作を決められる（マジックメソッド）
+  // 参考： https://www.php.net/manual/ja/language.oop5.magic.php#object.tostring
+  public function __toString(): string
+  {
+    return $this->value;
   }
 }
--- withPHP/user/main.php
+++ withPHP/user/main.php
@@ -7,16 +7,19 @@ spl_autoload_register(function($className) {
 
 // マルチバイト文字を含む文字列のTrim処理
 // 参考： https://qiita.com/fallout/items/a13cebb07015d421fde3
-function mbTrim(string $pString): ?string
+function mb_trim(string $pString): ?string
 {
     return preg_replace('/\A[\p{Cc}\p{Cf}\p{Z}]++|[\p{Cc}\p{Cf}\p{Z}]++\z/u', '', $pString);
 }
 
 // テストコード｜UserName
-$input = mbTrim('ユーザー1');
+$input = mb_trim('ユーザー1');
 // $input = mbTrim('ユー');
 // $input = mbTrim('');
 
 $username = new UserName($input);
-var_dump($username->Value());
+$message = $username . "さん、こんにちは。";
+print($message.PHP_EOL);
 
+// print(gettype($username));
+// print(gettype($message));
